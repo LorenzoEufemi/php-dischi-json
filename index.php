352dda1594efd99d4cd389ecf677bb390a153af2
LorenzoEufemi <lorenzoeufemi@gmail.com>
2025-03-17 14:47:54 +0100

    fixbug: aggiunta style card

--- index.php
+++ index.php
@@ -12,25 +12,29 @@ $dischi = getDischi();
     <style>
         body { font-family: Arial, sans-serif; text-align: center; }
         .disco { margin: 20px; display: inline-block; text-align: left; border: 1px solid #ccc; padding: 10px; border-radius: 5px; width: 200px;}
-        #dischi-container { display: flex; flex-wrap: wrap; justify-content: center; }
-        img { width: 150px; height: auto; }
+        
+        img { width: 100%; height: auto; }
     </style>
 </head>
 <body>
     <h1>Lista Dischi Musicali</h1>
-    <div id="dischi-container">
+    <div class="row row-cols-3 mb-4">
         <?php foreach ($dischi as $disco): ?>
             <div class="disco">
+                <div class="card-img">
                 <img src="<?= htmlspecialchars($disco['cover']) ?>" alt="<?= htmlspecialchars($disco['titolo']) ?>">
-                <h3><?= htmlspecialchars($disco['titolo']) ?></h3>
-                <p><?= htmlspecialchars($disco['artista']) ?> (<?= htmlspecialchars($disco['anno']) ?>)</p>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title"><?= htmlspecialchars($disco['titolo']) ?></h3>
+                <p class="card-text"><?= htmlspecialchars($disco['artista']) ?> (<?= htmlspecialchars($disco['anno']) ?>)</p>
+            </div>
             </div>
         <?php endforeach; ?>
     </div>
 
     <h2>Aggiungi un nuovo disco</h2>
     <div class="border">
-    <form class="" action="server.php" method="POST">
+    <form class="form d-flex flex-column justify-content-center align-items-center" action="server.php" method="POST">
         
         <input class="form-control" type="text" name="titolo" id="titolo" placeholder="Titolo" required>
         <label class ="form-label" for="titolo">Titolo</label><br>
